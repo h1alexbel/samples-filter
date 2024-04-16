3993e0c109802c61c5674dc4f753053ad39cb3be
h1alexbel <hialexbel@gmail.com>
2024-04-16 19:47:42 +0300

    chore(#1): puzzles

--- objects/__init__.py
+++ objects/__init__.py
@@ -1,23 +1,25 @@
-"""Top-level package for RP To-Do."""
-# rptodo/__init__.py
+# The MIT License (MIT)
+#
+# Copyright (c) 2024 Aliaksei Bialiauski
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import pkg_resources
 
-__app_name__ = "rptodo"
-__version__ = "0.1.0"
-
-(
-    SUCCESS,
-    DIR_ERROR,
-    FILE_ERROR,
-    DB_READ_ERROR,
-    DB_WRITE_ERROR,
-    JSON_ERROR,
-    ID_ERROR,
-) = range(7)
-
-ERRORS = {
-    DIR_ERROR: "config directory error",
-    FILE_ERROR: "config file error",
-    DB_READ_ERROR: "database read error",
-    DB_WRITE_ERROR: "database write error",
-    ID_ERROR: "to-do id error",
-}
+name = "samples-filter"
+version = pkg_resources.get_distribution("samples-filter").version
--- objects/cli.py
+++ objects/cli.py
@@ -1,18 +1,45 @@
-"""This module provides the RP To-Do CLI."""
-# rptodo/cli.py
-
+# The MIT License (MIT)
+#
+# Copyright (c) 2024 Aliaksei Bialiauski
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 from typing import Optional
 
 import typer
 
-from objects import __app_name__, __version__
+from objects import name, version
 
 app = typer.Typer()
 
+
+# @todo #1:45min Add support for --repositories and --out parameter.
+#  We should add support to our cli to handle two options: --repositories,
+#  and --out. Both indicate the name of the file to read/create.
+# @todo #1:25min Clean parameters that we don't need.
+#  Let's remove parameters that we don't really need in this cli.
+#  Less parameters we will have, the better.
+#  Don't forget to remove this puzzle.
 def _version_callback(value: bool) -> None:
     if value:
-        typer.echo(f"{__app_name__} v{__version__}")
+        typer.echo(f"{name} v{version}")
         raise typer.Exit()
+
 
 @app.callback()
 def main(
