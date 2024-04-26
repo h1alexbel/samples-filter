b31b81e6d5e7d4bf99d33cf844e51efc31edd215
h1alexbel <aliaksei.bialiauski@hey.com>
2024-04-26 14:45:18 +0300

    feat(#18): inference creates valid predictor

--- model/inference.py
+++ model/inference.py
@@ -1,4 +1,4 @@
 from predictor import Predictor
 
-prediction = Predictor("invan/spring3-mvc-maven-json-hello-world").predict()
+prediction = Predictor().predict("invan/spring3-mvc-maven-json-hello-world")
 print(prediction)
