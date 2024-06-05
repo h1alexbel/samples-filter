fd9e257ef3afab815e7138404bf773bbee406255
h1alexbel <aliaksei.bialiauski@hey.com>
2024-06-05 19:00:47 +0300

    feat(#143): puzzles

---
+++ models/model/isolation-forest.py
@@ -0,0 +1,6 @@
+# Train model with isolation forest algorithm.
+# @todo #129:90min Train isolation forest model.
+#  We need to find out how to properly train isolation forest model with
+#  gathered data in order to detect anomalies (SRs). Don't forget to remove
+#  this puzzle.
+print("training IF model...")
---
+++ models/model/t_bert.py
@@ -0,0 +1,6 @@
+# Train model based on BERT.
+# @todo #129:90min Train BERT transformer model.
+#  We need to find out how to properly train BERT model with
+#  gathered data in order to detect anomalies (SRs). Don't forget to remove
+#  this puzzle.
+print("training BERT based model...")
